02ea2c704e0d371dd6564976922224b1dac3a934
ferreirafernandar <ferreirafernandar@gmail.com>
2018-07-02 10:33:04 -0300

    Correção no teste

--- test/CurrencyConverter.API.Test/ServiceApiTest.cs
+++ test/CurrencyConverter.API.Test/ServiceApiTest.cs
@@ -1,4 +1,5 @@
-﻿using CurrencyConverter.API.Services;
+﻿using System.Collections.Generic;
+using CurrencyConverter.API.Services;
 using CurrencyConverter.API.Utils;
 using Moq;
 using Shared;
@@ -13,7 +14,6 @@ namespace UnitTest.CurrencyLayer.Application
             Singleton.AppSettings.BaseUrl = "";
         }
 
-
         [Theory]
         [InlineData("BRL", "USD")]
         [InlineData("USD", "BRL")]
@@ -21,6 +21,7 @@ namespace UnitTest.CurrencyLayer.Application
         [InlineData("BRL", "EUR")]
         [InlineData("USD", "EUR")]
         [InlineData("EUR", "USD")]
+
         public void Get_currency_converterlayer_success(string from, string to)
         {
             var mock = new Mock<IHttpClient>();
@@ -36,12 +37,12 @@ namespace UnitTest.CurrencyLayer.Application
         public void Get_currencies_converterlayer_success()
         {
             var mock = new Mock<IHttpClient>();
-            mock.Setup(x => x.GetBaseHttpTask<decimal>(Singleton.AppSettings.BaseUrl, "currency/currencies"));
+            mock.Setup(x => x.GetBaseHttpTask<Dictionary<string, string>>(Singleton.AppSettings.BaseUrl, "currency/currencies"));
 
             var service = new CurrencyConverterService(mock.Object);
 
             service.GetCurrencies();
-            mock.Verify(x => x.GetBaseHttpTask<decimal>(Singleton.AppSettings.BaseUrl, "currency/currencies"), Times.Once);
+            mock.Verify(x => x.GetBaseHttpTask<Dictionary<string, string>>(Singleton.AppSettings.BaseUrl, "currency/currencies"), Times.Once);
         }
     }
 }
