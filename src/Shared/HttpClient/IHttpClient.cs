2384cbf21f4a0b580c9ec1cb77bb3927411fc0dd
ferreirafernandar <ferreirafernandar@gmail.com>
2018-07-02 08:26:05 -0300

    Initial Commit

--- src/Shared/HttpClient/IHttpClient.cs
+++ src/Shared/HttpClient/IHttpClient.cs
@@ -2,6 +2,6 @@
 {
     public interface IHttpClient
     {
-        T GetBaseHttpTask<T>(string url, string endpoint, params string[] parameters);
+        T GetBaseHttpTask<T>(string url, string endpoint, params string[] parameters); 
     }
 }
